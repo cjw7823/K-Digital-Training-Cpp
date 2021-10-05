fe39d456bf9c1d9ab397e8ed3b0d1e9354d9f831
cjw7823 <cjw7823@naver.com>
2021-10-05 15:27:55 +0900

    Class 설명. 포인터 조금.

---
+++ 2021-10-05/Project1/Engine.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include "Engine.h"
+#include "Player.h"
+#include "Map.h"
+
+using namespace std;
+
+Engine::Engine()
+{
+	cout << "엔진이 생성 되었습니다." << endl;
+	Initialize();
+}
+
+Engine::~Engine()
+{
+	Terminalize();
+	cout << "엔진이 파괴 되었습니다." << endl;
+}
+
+void Engine::Initialize()
+{
+	cout << "엔진이 초기화 되었습니다." << endl;
+}
+
+void Engine::Run()
+{
+	while (true)
+	{
+		Input();
+		Process();
+		Render();
+	}
+}
+
+void Engine::Terminalize()
+{
+	cout << "종료 되었습니다." << endl;
+}
+
+void Engine::Input()
+{
+	std::cout << "입력받는다." << std::endl;
+}
+
+void Engine::Process()
+{
+	std::cout << "처리한다." << std::endl;
+}
+
+void Engine::Render()
+{
+	std::cout << "렌더링한다." << std::endl;
+}
---
+++ 2021-10-05/Project1/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include"Player.h"
+#include"Engine.h"
+#include"Map.h"
+
+using namespace std;
+
+
+int main()
+{
+	Engine engine;
+
+	engine.Run();
+
+}
