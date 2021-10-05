cdaebf4abc934292e0aee3ac01fd673bbaad685e
cjw7823 <cjw7823@naver.com>
2021-10-05 13:05:03 +0900

    첫 수업. Git 연습.

---
+++ 2021-09-14/CPP/second/second/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>//기본 입출력 함수를 사용하겠다.
+
+using namespace std;//기본 namespace 설정
+int main()
+{
+	cout << "*****\n";
+	cout << "****\n";
+	cout << "***\n";
+	cout << "**\n";
+	cout << "*\n";
+
+	return 0;
+
+}
