3b1d73110c8b010694085ece22eea81aa0a61c3f
cjw7823 <cjw7823@naver.com>
2021-10-05 13:07:00 +0900

    이항, 단항연산자 연습

---
+++ 2021-09-17/practice1/source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//int num1 = 0, num2 = 0;
+	//
+	//cout << "번호를 두 개 입력하세요.\n";
+
+	//cout << "첫번째 번호:";
+	//cin >> num1;
+
+	//cout << "두번째 번호:";
+	//cin >> num2;
+
+	//cout << "입력하신 두 번호의 합은 " << num1 + num2 << " 입니다.\n";
+
+	int a = 0;
+	a = a + 1;
+	a++;
+
+	cout << a;
+
+
+	return 0;
+}
